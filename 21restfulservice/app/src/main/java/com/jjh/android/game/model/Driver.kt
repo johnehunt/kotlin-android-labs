58a96875b05cc11b163a26b203abcc6074303b9f
John Hunt <john.hunt10@gmail.com>
2021-02-19 17:20:34 +0000

    Updates

---
+++ 21restfulservice/app/src/main/java/com/jjh/android/game/model/Driver.kt
@@ -0,0 +1,11 @@
+package com.jjh.android.game.model
+
+data class Driver(val driverId: String,
+                  val permanentNumber: String,
+                  val code: String,
+                  val url: String,
+                  val givenName: String,
+                  val familyName: String,
+                  val dateOfBirth: String,
+                  val nationality: String
+)
