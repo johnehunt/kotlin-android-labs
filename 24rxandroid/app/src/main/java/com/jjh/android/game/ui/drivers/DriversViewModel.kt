36c5d5ca66dac432043811f1d2ad678185ed9a2d
John Hunt <john.hunt10@gmail.com>
2021-02-22 15:47:42 +0000

    SOlution to lab 24

--- 24rxandroid/app/src/main/java/com/jjh/android/game/ui/drivers/DriversViewModel.kt
+++ 24rxandroid/app/src/main/java/com/jjh/android/game/ui/drivers/DriversViewModel.kt
@@ -1,6 +1,5 @@
 package com.jjh.android.game.ui.drivers
 
-import android.os.AsyncTask
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,11 +9,14 @@ import okhttp3.Request
 import okhttp3.Response
 
 import com.jjh.android.game.model.Driver
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class DriversViewModel : ViewModel() {
 
     companion object {
-        private const val TAG = "SlideshowViewModel"
+        private const val TAG = "DriversViewModel"
         private const val URL = "http://ergast.com/api/f1/"
         private val GSON = Gson()
     }
@@ -22,44 +24,39 @@ class DriversViewModel : ViewModel() {
     val drivers: MutableLiveData<List<Driver>> = MutableLiveData<List<Driver>>()
 
     fun loadDriverData(year: Int) {
-        Log.d(TAG, "loadDriverData")
-        GetRequestAsyncTask(year).execute()
-    }
+        Log.d(TAG, "loadDriverData($year)")
 
-    inner class GetRequestAsyncTask(val year: Int) : AsyncTask<Unit, Unit, String>() {
-
-        override fun doInBackground(vararg params: Unit): String {
-            Log.d(TAG, "GetRequestAsyncTask.doInBackground")
+        Observable.create<String> {
             val client = OkHttpClient()
             val urlString = "$URL$year/drivers.json"
             val request: Request = Request.Builder().url(urlString).get().build()
-
-            return try {
+            val result = try {
                 val response: Response = client.newCall(request).execute()
                 response.body?.string() ?: ""
             } catch (e: Exception) {
                 Log.d(TAG, e.localizedMessage)
                 ""
             }
-        }
-
-        override fun onPostExecute(result: String) {
-            super.onPostExecute(result)
-            Log.d(TAG, "GetRequestAsyncTask.onPostExecute")
-            if (result.isNotEmpty() && !result.contains("Bad Request")) {
-                val startIndex = result.indexOf("\"Drivers\":") + "\"Drivers\":".length
-                val driversJson = result.slice(IntRange(startIndex, result.length - 4))
-                Log.d(TAG, driversJson)
-                driversJson.run {
-                    // Example of converting JSON to an object
-                    val driversList: List<Driver> =
-                        GSON.fromJson(this, Array<Driver>::class.java)
-                            .toList()
-                    drivers.value = driversList
+            it.onNext(result)
+            it.onComplete()
+        }.subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                Log.d(TAG, it)
+                if (it.isNotEmpty() && !it.contains("Bad Request")) {
+                    val startIndex = it.indexOf("\"Drivers\":") + "\"Drivers\":".length
+                    val driversJson = it.slice(IntRange(startIndex, it.length - 4))
+                    Log.d(TAG, driversJson)
+                    driversJson.run {
+                        // Convert JSON to an object
+                        val driversList: List<Driver> =
+                            GSON.fromJson(this, Array<Driver>::class.java)
+                                .toList()
+                        Log.d(TAG, driversList.toString())
+                        drivers.value = driversList
+                    }
                 }
             }
-        }
-
     }
 
 }
