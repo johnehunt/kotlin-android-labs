41c03a5d6c3f979e0515a2a14fde5f05caec2f90
John Hunt <john.hunt10@gmail.com>
2021-02-23 13:46:21 +0000

    Added link to new room based functionality

--- 25room/app/src/main/java/com/jjh/android/game/ui/gallery/GalleryFragment.kt
+++ 25room/app/src/main/java/com/jjh/android/game/ui/gallery/GalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.jjh.android.game.ui.gallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,25 +9,50 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jjh.android.game.R
-
+import com.jjh.android.game.db.HeroRepository
+import com.jjh.android.game.ui.gallery.HeroDialog.HeroDialogListener
 import kotlinx.android.synthetic.main.fragment_gallery.*
 
 class GalleryFragment : Fragment() {
 
+    companion object {
+        private const val TAG = "GalleryFragment"
+    }
+
     private val galleryViewModel by viewModels<GalleryViewModel>()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_gallery, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        hero_recycler_view.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = HeroAdapter(galleryViewModel)
+        Log.d(TAG, "onViewCreated")
+
+        galleryViewModel.repository = HeroRepository(requireActivity().application)
+        galleryViewModel.refresh().subscribe {
+            hero_recycler_view.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = HeroAdapter(galleryViewModel)
+            }
         }
+
+        addButton.setOnClickListener {
+            HeroDialog(activity, object : HeroDialogListener {
+                override fun onOK(id: Int, name: String, guess: Int) {
+                    Log.d(TAG, "The user tapped OK, input is $id $name $guess")
+                    val hero = Hero(id, name, guess)
+                    (hero_recycler_view.adapter as HeroAdapter).addHero(hero)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "The user tapped Cancel")
+                }
+            }).show()
+        }
+
     }
 }
