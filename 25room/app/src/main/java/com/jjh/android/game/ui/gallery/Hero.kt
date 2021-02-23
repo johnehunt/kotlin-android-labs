280643ab3bc447896cbe82ccd5cc4c3f3e4bd168
John Hunt <john.hunt10@gmail.com>
2021-02-23 12:12:39 +0000

    Added ROOM annotations

--- 25room/app/src/main/java/com/jjh/android/game/ui/gallery/Hero.kt
+++ 25room/app/src/main/java/com/jjh/android/game/ui/gallery/Hero.kt
@@ -1,3 +1,12 @@
 package com.jjh.android.game.ui.gallery
 
-data class Hero(val name: String, val guesses: Int)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "heroes")
+data class Hero(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    @ColumnInfo(name = "guesses_taken") val guesses: Int
+)
